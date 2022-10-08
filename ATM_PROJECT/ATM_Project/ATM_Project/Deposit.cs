71ac30cc9b4d762757f781242be82e5471ac2843
FilipFalinski <55143687+FilipFalinski@users.noreply.github.com>
2022-10-08 17:50:09 +0100

    Add files via upload

---
+++ ATM_PROJECT/ATM_Project/ATM_Project/Deposit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ATM_Project
+{
+    public partial class Deposit : Form
+    {
+        public Deposit()
+        {
+            InitializeComponent();
+        }
+    }
+}
