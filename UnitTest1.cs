0cee1c3ea0b2cfbd1263c88edef1c01366350d54
FilipFalinski <55143687+FilipFalinski@users.noreply.github.com>
2022-11-15 15:36:23 +0000

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+using ATM_Project;
+using System;
+using Xunit;
+
+namespace ATMtest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void test1()
+        {
+           
+             
+             
+        }
+    }
+}
